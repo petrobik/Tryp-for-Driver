966b870bcf434c3db41014d783a1900915eed666
kariyHub <kariydm99@gmail.com>
2019-03-23 21:46:18 +0200

    fix bug with location

--- app/src/main/java/com/rdev/trypfordriver/ui/map/BearingInterpolator.java
+++ app/src/main/java/com/rdev/trypfordriver/ui/map/BearingInterpolator.java
@@ -12,23 +12,28 @@ public interface BearingInterpolator {
         public float interpolate(float fraction, float lastbearing, LatLng from, LatLng to) {
             // http://en.wikipedia.org/wiki/Slerp
             Log.d("tag", "fraction" + fraction);
-            float deduction = (float) (angleFromCoordinate(from.latitude, from.longitude, to.latitude, to.longitude) - lastbearing);
+            float deduction = (float) (angleFromCoordinate(from, to) - lastbearing);
             return lastbearing + deduction * fraction;
         }
 
-        private static double angleFromCoordinate(double lat1, double long1, double lat2,
-                                                  double long2) {
+        private static double angleFromCoordinate(LatLng latLng1, LatLng latLng2) {
+            double PI = 3.14159;
+            double lat1 = latLng1.latitude * PI / 180;
+            double long1 = latLng1.longitude * PI / 180;
+            double lat2 = latLng2.latitude * PI / 180;
+            double long2 = latLng2.longitude * PI / 180;
+
             double dLon = (long2 - long1);
 
             double y = Math.sin(dLon) * Math.cos(lat2);
             double x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1)
                     * Math.cos(lat2) * Math.cos(dLon);
 
-            double brng = Math.atan2(x, y);
+            double brng = Math.atan2(y, x);
 
             brng = Math.toDegrees(brng);
-            brng = (brng + 360) % 360;
-            brng = 180 - brng;
+            brng = (brng + 90) % 360;
+
             return brng;
         }
     }
